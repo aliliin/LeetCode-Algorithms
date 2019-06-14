148a430abe50099c42e64423c31e525054de2456
高永立 <phperali@gmail.com>
2019-06-14 10:15:02 +0800

    冒泡排序

---
+++ 冒泡排序/bubbleSort.php
@@ -0,0 +1,21 @@
+<?php
+
+$arr = array(1, 43, 54, 62, 21, 66, 32, 78, 36, 76, 39);
+function bubbleSort($arr)
+{
+    $len = count($arr);
+    //该层循环控制 需要冒泡的轮数
+    for ($i = 1; $i < $len; $i++) {
+        //该层循环用来控制每轮 冒出一个数 需要比较的次数
+        for ($k = 0; $k < $len - $i; $k++) {
+            if ($arr[$k] > $arr[$k + 1]) {
+                $tmp         = $arr[$k + 1]; // 声明一个临时变量
+                $arr[$k + 1] = $arr[$k];
+                $arr[$k]     = $tmp;
+            }
+        }
+    }
+    return $arr;
+}
+echo '<pre/>';
+print_r(bubbleSort($arr));
