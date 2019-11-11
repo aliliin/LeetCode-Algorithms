3893fa4b3e9e88f626e78f611bb4e0da1de8d96c
高永立 <phperali@gmail.com>
2019-11-11 11:25:17 +0800

    二叉树的最小深度

---
+++ 二叉树的最小深度/Solution.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ 给定一个二叉树，找出其最小深度。
+
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例:
+
+给定二叉树 [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最小深度  2.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-depth-of-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+    /**
+     *
+     * 递归方法。
+     */
+    function minDepth($root) {
+        if($root == null) return 0;
+        $left = $this->minDepth($root->left);
+        $right = $this->minDepth($root->right);
+        return ($left == 0 || $right == 0) ? $left +$right +1:min($left,$right)+1;
+    }
+}
