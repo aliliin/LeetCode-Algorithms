366686e1210f1c1107a7a9d61539fa4c7a6c760e
高永立 <phperali@gmail.com>
2019-12-18 16:53:08 +0800

    删除排序数组中的重复项

--- 删除排序数组中的重复项/removeDuplicates.php
+++ 删除排序数组中的重复项/removeDuplicates.php
@@ -1,8 +1,7 @@
 <?php
 
-
-$nums = [1, 1, 2];
-$Solution = new  Solution();
+$nums     = [1, 1, 2];
+$Solution = new Solution();
 var_dump($Solution->removeDuplicates1($nums));
 var_dump($Solution->removeDuplicates($nums));
 var_dump($Solution->removeDuplicates2($nums));
@@ -11,9 +10,12 @@ var_dump($Solution->removeDuplicates3($nums));
 class Solution
 {
 
-    function removeDuplicates(&$nums)
+    public function removeDuplicates(&$nums)
     {
-        if (count($nums) == 0) return 0;
+        if (count($nums) == 0) {
+            return 0;
+        }
+
         $i = 0;
         foreach ($nums as $num) {
             if ($nums[$i] != $num) {
@@ -24,24 +26,13 @@ class Solution
         return $i + 1;
     }
 
-
-    function removeDuplicates1(&$nums)
-    {
-        if (count($nums) == 0) return 0;
-        $i = 1;
-        foreach ($nums as $num) {
-            if ($nums[$i] == $num) {
-                unset($nums[$i]);
-                $i++;
-            }
-        }
-        return count($nums);
-    }
-
-    function removeDuplicates2(&$nums)
+    public function removeDuplicates2(&$nums)
     {
         $len = count($nums);
-        if ($len == 0) return 0;
+        if ($len == 0) {
+            return 0;
+        }
+
         $count = 0;
         for ($i = 1; $i < $len; $i++) {
             if ($nums[$i] == $nums[$i - 1]) {
@@ -53,10 +44,13 @@ class Solution
         return $len - $count;
     }
 
-    function removeDuplicates3(&$nums)
+    public function removeDuplicates3(&$nums)
     {
         $len = count($nums);
-        if ($len == 0) return 0;
+        if ($len == 0) {
+            return 0;
+        }
+
         for ($i = 0; $i < $len - 1; $i++) {
             if ($nums[$i] == $nums[$i + 1]) {
                 unset($nums[$i]);
