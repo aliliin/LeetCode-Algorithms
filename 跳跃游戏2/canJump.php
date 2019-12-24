728501232647ca7ef128ffcbf5da7b14ccd1c4bb
高永立 <phperali@gmail.com>
2019-12-24 11:14:11 +0800

    跳跃游戏2

--- 跳跃游戏2/canJump.php
+++ 跳跃游戏2/canJump.php
@@ -1,1 +1,33 @@
 <?php
+
+
+$Solution = new Solution();
+$nums = [2, 3, 1, 1, 4];
+
+var_dump($Solution->canJump($nums));
+
+class Solution
+{
+
+    /**
+     * 贪心算法
+     */
+    function canJump($nums)
+    {
+        if (empty($nums)) return false;
+        $end = 0;
+        $maxPosition = 0;
+        $step = 0;
+        for ($i = 0; $i < count($nums) - 1; $i++) {
+            // 找到跳的最远的
+            $maxPosition = max($maxPosition, $nums[$i] + $i);
+            // 遇到边界就更新边界，并且 步数加1 ¥step +1
+            if ($i == $end) {
+                $end = $maxPosition;
+                $step++;
+            }
+        }
+        return $step;
+    }
+
+}
