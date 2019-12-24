4d801f816117ce8571f6afc797e76bcdd7ae129f
高永立 <phperali@gmail.com>
2019-12-24 11:13:44 +0800

    跳跃游戏

--- 跳跃游戏/canJump.php
+++ 跳跃游戏/canJump.php
@@ -1,1 +1,36 @@
 <?php
+
+$Solution = new Solution();
+$nums = [2, 3, 1, 1, 4];
+
+var_dump($Solution->canJump($nums));
+var_dump($Solution->canJump1($nums));
+
+class Solution
+{
+
+    /**
+     * 贪心算法
+     */
+    function canJump($nums)
+    {
+        if (empty($nums)) return false;
+        $end = count($nums) - 1;
+        for ($i = $end; $i >= 0; $i--) {
+            if ($nums[$i] + $i >= $end) {
+                $end = $i;
+            }
+        }
+        return $end == 0;
+    }
+
+    function canJump1($nums)
+    {
+        $res = 0;
+        foreach ($nums as $k => $num) {
+            if ($k > $res) return false;
+            if ($num + $k > $res) $res = $num + $k;
+        }
+        return $res >= count($nums) - 1;
+    }
+}
