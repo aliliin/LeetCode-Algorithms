3e6bfb874b344751167cc2c3dd77fa0d708151d4
高永立 <phperali@gmail.com>
2019-12-24 11:14:44 +0800

    柠檬水找零

--- 柠檬水找零/lemonadeChange.php
+++ 柠檬水找零/lemonadeChange.php
@@ -1,1 +1,67 @@
 <?php
+
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $bills
+     * @return Boolean
+     */
+    function lemonadeChange($bills)
+    {
+        $five = 0;
+        $ten = 0;
+        foreach ($bills as $bill) {
+            if ($bill == 5) {
+                $five += 1;
+            } elseif ($bill == 10) {
+                if ($five == 0) return false;
+                $five -= 1;
+                $ten += 1;
+            } else {
+                if ($five > 0 && $ten > 0) {
+                    $five -= 1;
+                    $ten -= 1;
+                } elseif ($five >= 3) {
+                    $five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    function lemonadeChange1($bills)
+    {
+        $five = 0;
+        $ten = 0;
+        foreach ($bills as $bill) {
+            if ($bill == 5) {
+                $five += 1;
+            } elseif ($bill == 10) {
+                if ($five == 0) return false;
+                $five -= 1;
+                $ten += 1;
+            } else {
+                if ($five > 0 && $ten > 0) {
+                    $ten -= 1;
+                    $five -= 1;
+                } elseif ($five >= 3) {
+                    $five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+$Solution = new Solution();
+$bills = [5, 5, 5, 10, 5, 5, 10, 20, 20, 20];
+$bills = [5, 5, 10, 10, 20];
+// [5,5,10,10,20];
+print_r($Solution->lemonadeChange1($bills));
+print_r($Solution->lemonadeChange($bills));
