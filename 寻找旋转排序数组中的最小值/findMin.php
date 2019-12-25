b0557c555ed14fe7862d0ce3a6f8a721ca0bd664
高永立 <phperali@gmail.com>
2019-12-25 10:32:08 +0800

    寻找旋转排序数组中的最小值

--- 寻找旋转排序数组中的最小值/findMin.php
+++ 寻找旋转排序数组中的最小值/findMin.php
@@ -1,1 +1,37 @@
 <?php
+
+$nums = [3, 4, 5, 1, 2];
+$nums = [4, 5, 6, 7, 0, 1, 2];
+$Solution = new Solution();
+var_dump($Solution->findMin($nums));
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    public function findMin($nums)
+    {
+        if (empty($nums)) return 0;
+        $left = 0;
+        $right = count($nums) - 1;
+        while ($left <= $right) {
+            $mid = floor(($left + $right) / 2);
+            if ($nums[$mid] > $nums[$mid + 1]) return $nums[$mid + 1];
+            if ($nums[$mid] < $nums[$mid - 1]) return $nums[$mid];
+            if ($nums[0] < $nums[$mid]) {
+                $left = $mid + 1;
+            } else {
+                $right = $mid - 1;
+            }
+        }
+        return null;
+    }
+
+    function findMin1($nums)
+    {
+        return min($nums);
+    }
+}
