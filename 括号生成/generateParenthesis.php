c2a08fefdda2f3befb98206d674678eee261670e
高永立 <phperali@gmail.com>
2020-01-15 12:11:38 +0800

    括号生成/

--- 括号生成/generateParenthesis.php
+++ 括号生成/generateParenthesis.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
 
@@ -30,7 +31,7 @@ class Solution
     }
 
     /**
-     * 新建设 递归方法
+     * 新建设 递归方法 （剪枝）
      * @param $left 左括号数量
      * @param $right 又括号数量
      * @param $n
@@ -45,9 +46,7 @@ class Solution
 
         if ($left < $n) {
             $this->_generateParenthesis($left + 1, $right, $n, $result . "(");
-
         }
-
 
         if ($left > $right and $right < $n) {
             $this->_generateParenthesis($left, $right + 1, $n, $result . ")");
@@ -76,10 +75,39 @@ class Solution
         }
         return $this->list;
     }
+
+    // 动态规划
+    function generateParenthesis1($n)
+    {
+        if ($n == 0) {
+            return [];
+        }
+        if ($n == 1) {
+            return ["()"];
+        }
+        $dp = [];
+        $dp[0][] = "";
+        $dp[1][] = "()";
+        for ($i = 2; $i < $n + 1; $i++) {
+            for ($j = 0; $j < $i; $j++) {
+                for ($p = 0; $p < count($dp[$j]); $p++) {
+                    for ($q = 0; $q < count($dp[$i - $j - 1]); $q++) {
+                        $dp[$i][] = "(" . $dp[$j][$p] . ")" . $dp[$i - $j - 1][$q];
+                    }
+                }
+            }
+        }
+        return $dp[$n];
+    }
 }
 
 $sulution = new Solution();
 $n = 3;
+$res = $sulution->generateParenthesis1($n);
+var_dump($res);
+die;
+die;
+
 $res = $sulution->generateParenthesis($n);
 $res1 = $sulution->gen($n);
 print_r($res);
